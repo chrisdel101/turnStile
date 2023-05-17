8b3c634a4787477e6061fad6a0ef6a6c1a90e841
Chris Del <drumgod101@gmail.com>
2023-05-17 16:42:26 -0600

    Add embedded schema

--- lib/turnStile/end_users/models/organization.ex
+++ lib/turnStile/end_users/models/organization.ex
@@ -1,23 +1,49 @@
 defmodule TurnStile.Company.Organization do
   use Ecto.Schema
   import Ecto.Changeset
-  alias TurnStile.Repo
 
 
   schema "organizations" do
-    field :email, :string
+    field :org_email, :string
     field :name, :string
     field :slug, :string
     field :phone, :string
     has_many :employee, TurnStile.Staff.Employee
     has_many :owner, TurnStile.Staff.Owner
+
+    embeds_one :owner_user, TurnStile.Staff.Employee do
+      field :first_name, :string
+      field :last_name, :string
+      field :email, :string
+      field :password, :string, virtual: true, redact: true
+    end
     timestamps()
+
+
   end
 
   @doc false
   def changeset(organization, attrs) do
     organization
-    |> cast(attrs, [:name, :email, :phone, :slug])
+    |> cast(attrs, [:name, :org_email, :phone, :slug])
     |> validate_required([:name, :slug])
+    |> cast_embed(:owner_user, required: true, with: &owner_user_changeset/2)
+  end
+
+  def owner_user_changeset(owner_user_changeset, attrs \\ %{}) do
+    owner_user_changeset
+    |> cast(attrs, [:first_name, :last_name, :email, :password])
+    |> validate_required([:first_name, :last_name, :email, :password])
+    |> validate_email()
+  end
+
+  defp validate_email(changeset) do
+    IO.inspect(changeset)
+    changeset
+    |> validate_required([:email])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+    |> validate_length(:email, max: 160)
+    |> validate_confirmation(:email, message: "Emails do not match")
+    |> validate_confirmation(:password, message: "Passwords do not match")
   end
 end
