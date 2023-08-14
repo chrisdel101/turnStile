1688763869ff625fba70069772b06693b341af95
Chris Del <drumgod101@gmail.com>
2023-08-13 23:29:42 -0600

    Revert "Revert "put controllers into folders; delete unused user controllers""
    This reverts commit 89a9522a08947a44fc8e1e5c73a2f47e3a6fd799.

---
+++ lib/turnStile_web/controllers/organizations/organization_controller.ex
@@ -0,0 +1,354 @@
+defmodule TurnStileWeb.OrganizationController do
+  use TurnStileWeb, :controller
+
+  alias TurnStileWeb.EmployeeRegistrationController
+  alias TurnStile.Staff
+  alias TurnStile.Company
+  alias TurnStile.Company.Organization
+  alias TurnStileWeb.EmployeeAuth
+  alias TurnStile.Staff.Employee
+
+  plug :track_form_stage
+
+  @moduledoc """
+  Controller for managing organizations
+  Flow: using multi-step form
+  -handled by track_form_stage on each call
+  1.init - render form 1 (new_org_form.html)
+  2.handle_new - handle submit form 1; display form 2 (new_employee_form.html)
+  3.handle_create - handle submit form 2; create org
+
+  """
+
+  def index(conn, _params) do
+    # no end users ever see this page
+    organizations = Company.list_organizations()
+    render(conn, "index.html", organizations: organizations)
+  end
+
+  # init - render form 1
+  def new(conn, _params) do
+    org_params = get_session(conn, :org_params)
+    # IO.inspect(org_params)
+    changeset = Company.change_organization(%Organization{}, org_params || %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  # second render - handle submuit form 1; display form 2
+  def handle_new(conn, %{"organization" => org_params}) do
+    changeset = Company.change_organization(%Organization{}, org_params)
+    # IO.inspect( changeset, label: "HERE222")
+    # make sure name is not empty
+    case Ecto.Changeset.get_change(changeset, :name) do
+      # handle empty name
+      value when value in [nil, ""] ->
+        # change changset action to insert
+        {:error, %Ecto.Changeset{} = changeset} = Ecto.Changeset.apply_action(changeset, :insert)
+        # re-render "new" again
+        render(conn, "new.html", changeset: changeset)
+
+      # when valid name field
+      _ ->
+        # extract name
+        %{"name" => name} = org_params
+        # make slug
+        slug = Slug.slugify(name)
+        # check DB for org
+        organizations? = Company.check_organization_exists_by_slug(slug)
+        org_params = Map.put_new(org_params, "slug", slug)
+        # handle duplicate org
+        if is_nil(organizations?) || length(organizations?) !== 0 do
+          conn
+          |> put_flash(:info, "That Organization already exists. Try another name.")
+          |> render("new.html", changeset: changeset)
+        else
+          current_employee = conn.assigns[:current_employee]
+
+          if current_employee do
+            employee_changeset = Ecto.Changeset.change(current_employee)
+            # add org params to sessions
+            conn = Plug.Conn.put_session(conn, :org_params, org_params)
+            # set from partial flag on conn
+            # conn = assign(conn, :org_form_submitted, true)
+            conn = track_form_stage(conn, nil, true)
+
+            render(conn, "new.html", changeset: employee_changeset)
+          else
+            employee_changeset = Staff.create_employee(%Employee{})
+
+            # add org params to sessions
+            conn = Plug.Conn.put_session(conn, :org_params, org_params)
+            # set from partial flag on conn
+            # conn = assign(conn, :org_form_submitted, true)
+            conn = track_form_stage(conn, nil, true)
+            render(conn, "new.html", changeset: employee_changeset)
+          end
+        end
+    end
+  end
+
+  # second render - handle same func w/ invalid params for error
+  def handle_new(conn, org_params) do
+    changeset = Company.change_organization(%Organization{}, org_params)
+
+    conn
+    |> put_flash(:error, "A parameter error ocurred. Cancel and retry.")
+    # re-render "new" again w error
+    |> render("new.html", changeset: changeset)
+  end
+
+  def create(conn, employee_params) do
+    IO.inspect(employee_params, label: "employee_params")
+    # extract params from session
+    current_employee = conn.assigns[:current_employee]
+    org_params = Map.get(get_session(conn), "org_params")
+
+    # add organization
+    case Company.insert_and_preload_organization(org_params) do
+      {:ok, organization} ->
+        IO.inspect(organization, label: "insert_and_preload_organization")
+        # creating initial employee
+        if !current_employee do
+          role =
+            TurnStile.Roles.build_role(%{
+              name: EmployeeRolesMap.get_permission_role("OWNER"),
+              value: to_string(EmployeeRolesMap.get_permission_role_value("OWNER"))
+            })
+          case EmployeeRegistrationController.create_initial_owner(
+            conn,
+            organization,
+            employee_params
+          ) do
+            {:error, error} ->
+              # delete any organization just saved
+              Company.delete_organization(organization)
+              IO.inspect(error, label: "ERROR in create orgnization_controller")
+              # conn = assign(conn, :org_form_submitted, true)
+              conn = track_form_stage(conn, nil, true)
+              conn
+              |> assign(:org_form_submitted, true)
+              |> put_flash(:error, "Error in Employee creation: Try again.")
+              |> render("new.html", changeset: error)
+
+            # create_initial_owner returns employee, log_in? bool
+            {:ok, employee, log_in?} ->
+              case TurnStile.Company.update_employee_assoc(organization, employee) do
+                {:ok, updated_org} ->
+                  IO.inspect(updated_org, label: "updated_org")
+                  # add has_many role assocations
+                  role = TurnStile.Roles.assocaiate_role_with_employee(role, employee)
+                  role = TurnStile.Roles.assocaiate_role_with_organization(role, updated_org)
+
+                  IO.inspect(employee, label: "employee")
+                  IO.inspect(updated_org, label: "updated_org")
+                  IO.inspect(role, label: "role")
+
+                  case TurnStile.Roles.insert_role(employee.id, updated_org.id, role) do
+                    {:error, error} ->
+                      # delete any prev created employees
+                      if !is_nil(employee) do
+                        Staff.delete_employee(employee)
+                      end
+                      {:error, error}
+
+                    {:ok, role} ->
+                      IO.inspect(role, label: "ROLE2")
+                      IO.inspect(log_in?)
+
+                      if log_in? === "true" do
+                        IO.inspect("OK TRUE")
+                        params = %{flash: "Organization Successfully created"}
+
+                        EmployeeAuth.log_in_employee_on_create(
+                          conn,
+                          employee,
+                          Map.get(organization, "id") || Map.get(organization, :id),
+                          Routes.organization_path(conn, :show, organization.id, %{
+                            "emptyParams" => true,
+                            "paramsKey" => "org_params"
+                          }),
+                          params
+                        )
+                      else
+                        # IO.inspect(updated_org)
+                        conn
+                        |> put_flash(
+                          :info,
+                          "An email was sent you your account. Please check your email to confirm your account. "
+                        )
+                        |> redirect(
+                          to:
+                            Routes.organization_path(conn, :show, organization.id, %{
+                              "emptyParams" => true,
+                              "paramsKey" => "org_params"
+                            })
+                        )
+                      end
+                  end
+
+                {:error, error} ->
+                  IO.inspect(error, label: "ERROR")
+                  # delete any added employee so far
+                  if !is_nil(employee) do
+
+                    Staff.delete_employee(employee)
+                  end
+                  conn
+                  |> assign(:org_form_submitted, true)
+                  |> put_flash(:error, "Error in assocaitions. Employee not created. Try again.")
+                  |> render("new.html", changeset: error)
+              end
+
+            #  default case; if runtime or unknown error, etc
+            _ ->
+              # delete any organization just saved
+              Company.delete_organization(organization)
+
+              error_msg = "ERROR in create orgnization_controller default case"
+              IO.inspect(error_msg)
+              # conn = assign(conn, :org_form_submitted, true)
+              conn
+              |> track_form_stage(nil, true)
+              |> put_flash(:error, error_msg)
+              |> render("new.html",
+                changeset: Employee.registration_changeset(%Staff.Employee{}, %{})
+              )
+          end
+
+          # adding existing employee to new org
+          # TODO: feature UNTESTED
+        else
+          # existing employee will be owner on new org
+          role =
+            TurnStile.Roles.build_role(%{
+              name: EmployeeRolesMap.get_permission_role("OWNER"),
+              value: to_string(EmployeeRolesMap.get_permission_role_value("OWNER"))
+            })
+
+          case TurnStile.Company.update_employee_assoc(organization, current_employee) do
+            {:ok, updated_org} ->
+              IO.inspect(updated_org, label: "updated_org")
+              # add has_many role assocations
+              role = TurnStile.Roles.assocaiate_role_with_employee(role, current_employee)
+              role = TurnStile.Roles.assocaiate_role_with_organization(role, updated_org)
+              IO.inspect("log_in?")
+
+              case TurnStile.Roles.insert_role(current_employee.id, updated_org.id, role) do
+                {:error, error} ->
+                  {:error, error}
+
+                {:ok, role} ->
+                  IO.inspect(role, label: "ROLE")
+                  # IO.inspect("OK")
+                  # IO.inspect(updated_org)
+                  conn
+                  |> put_flash(:info, "Organization Successfully created.")
+                  |> redirect(
+                    to:
+                      Routes.organization_path(conn, :show, organization.id, %{
+                        "emptyParams" => true,
+                        "paramsKey" => "org_params"
+                      })
+                  )
+              end
+
+            {:error, error} ->
+              IO.inspect("ERROR")
+
+              conn
+              |> track_form_stage(nil, true)
+              |> put_flash(:error, "Employee not created. Try again.")
+              |> render("new.html", changeset: error)
+          end
+        end
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        IO.inspect("CREATE ERROR")
+        IO.inspect(changeset)
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    IO.inspect("SHOW")
+    organization = Company.get_organization(id)
+
+    if !organization do
+      conn
+      |> put_flash(:info, "That Organization doesn't exist. Try again.")
+      |> redirect(to: Routes.organization_path(conn, :index))
+    else
+      members? = Company.organization_has_members?(organization.id)
+      # TODO: make a special login changeset
+      changeset = Staff.change_employee(%Employee{})
+
+      render(conn, "show.html",
+        organization: organization,
+        changeset: changeset,
+        members?: members?,
+        organization_id: organization.id,
+        error_message: nil
+      )
+    end
+  end
+
+  def edit(conn, %{"id" => id}) do
+    organization = Company.get_organization(id)
+    changeset = Company.change_organization(organization)
+    render(conn, "edit.html", organization: organization, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "organization" => organization_params}) do
+    organization = Company.get_organization(id)
+
+    case Company.update_organization(organization, organization_params) do
+      {:ok, organization} ->
+        conn
+        |> put_flash(:info, "Organization updated successfully.")
+        |> redirect(to: Routes.organization_path(conn, :show, organization))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", organization: organization, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    organization = Company.get_organization(id)
+    {:ok, _organization} = Company.delete_organization(organization)
+
+    conn
+    |> put_flash(:info, "Organization deleted successfully.")
+    |> redirect(to: Routes.organization_path(conn, :index))
+  end
+
+  # display search bar for organizations
+  def search_get(conn, _params) do
+    changeset = Company.change_organization(%Organization{})
+    organizations = Company.list_organizations()
+    render(conn, "search.html", changeset: changeset, organizations: organizations)
+  end
+
+  # execute search bar for organizations, direct to show page or show error
+  def search_post(conn, params) do
+    # slugigy param
+    slug = Slug.slugify(params["organization"]["name"] || "")
+    # check if org name exists
+    organization = Company.get_organization_by_name(slug)
+
+    if !organization do
+      conn
+      |> put_flash(:error, "No organization by that name.")
+      |> redirect(to: Routes.organization_path(conn, :search_get))
+    else
+      redirect(conn, to: Routes.organization_path(conn, :show, organization.id))
+    end
+  end
+
+  # plug
+  # sets flag to false unless manulally set to true
+  defp track_form_stage(conn, _opts, bool \\ false) do
+    # IO.inspect(bool, label: "bool")
+      conn
+      |> assign(:org_form_submitted, bool)
+  end
+end
---
+++ lib/turnStile_web/controllers/users/user_auth.ex
@@ -0,0 +1,315 @@
+defmodule TurnStileWeb.UserAuth do
+  use TurnStileWeb, :controller
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias TurnStile.Patients
+  alias TurnStile.Patients.UserToken
+  alias TurnStileWeb.Router.Helpers, as: Routes
+
+  @session_max_age_seconds UserToken.get_session_token_validity_seconds()
+  # used to control user session
+  @expiration_cookie "_turn_stile_web_user_expiration"
+  @expiration_me_options [sign: true, max_age: @session_max_age_seconds, same_site: "Lax"]
+
+  @doc """
+  Logs the user in.
+
+  It renews the session ID and clears the whole session
+  to avoid fixation attacks. See the renew_session
+  function to customize this behaviour.
+
+  It also sets a `:live_socket_id` key in the session,
+  so LiveView sessions are identified and automatically
+  disconnected on log out. The line can be safely removed
+  if you are not using LiveView.
+  """
+  def log_in_user(conn, user, params \\ %{}) do
+    {token, _token_struct} = Patients.build_and_insert_user_session_token(user)
+
+    conn
+    |> renew_session()
+    |> put_session(:user_token, token)
+    |> put_session(:live_socket_id, "users_sessions:#{Base.url_encode64(token)}")
+    |> maybe_write_expiration_cookie(token, params)
+  end
+
+  # if set to true make sure to put ensure_user_cookie_not_expired in routuer plug pipeline - currently no used
+  defp maybe_write_expiration_cookie(conn, token, %{"expirtation" => "true"}) do
+    put_resp_cookie(conn, @expiration_cookie, token, @expiration_me_options)
+  end
+
+  defp maybe_write_expiration_cookie(conn, _token, _params) do
+    conn
+  end
+
+  @doc """
+  Used for routes that require the user to be authenticated.
+
+  Mean user has activated email token and now is in timed session - no login required
+  """
+  def require_authenticated_user(conn, _opts) do
+    # current_user = conn.assigns[:current_user]
+    # IO.inspect(conn.assigns[:current_user], label: "current_user: require_authenticated_user")
+    if conn.assigns[:current_user] do
+      conn
+    else
+      IO.puts("require_authenticated_user: failed")
+
+      conn
+      |> put_flash(
+        :info,
+        "You're session is expired or you tried to access an authencated route."
+      )
+      |> maybe_store_return_to()
+      |> redirect(to: "/")
+      |> halt()
+    end
+  end
+
+  # if expired logs out user and redirects to index
+  def require_non_expired_user_session(conn, _params) do
+    if conn.assigns[:current_user] do
+      handle_validate_session_token(conn)
+    else
+      conn
+    end
+  end
+
+  # This function renews the session ID and erases the whole
+  # session to avoid fixation attacks. If there is any data
+  # in the session you may want to preserve after log in/log out,
+  # you must explicitly fetch the session data before clearing
+  # and then immediately set it after clearing, for example:
+  #
+  #     defp renew_session(conn) do
+  #       preferred_locale = get_session(conn, :preferred_locale)
+  #
+  #       conn
+  #       |> configure_session(renew: true)
+  #       |> clear_session()
+  #       |> put_session(:preferred_locale, preferred_locale)
+  #     end
+  #
+  defp renew_session(conn) do
+    conn
+    |> configure_session(renew: true)
+    |> clear_session()
+  end
+
+  @doc """
+  Logs the user out.
+
+  It clears all session data for safety. See renew_session.
+  """
+  def log_out_user(conn) do
+    user_token = get_session(conn, :user_token)
+    user_token && Patients.delete_session_token(user_token)
+
+    if live_socket_id = get_session(conn, :live_socket_id) do
+      TurnStileWeb.Endpoint.broadcast(live_socket_id, "disconnect", %{})
+    end
+
+    conn
+    |> renew_session()
+    |> delete_resp_cookie(@expiration_cookie)
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
+
+  @doc """
+  Logs the user out.
+
+  It clears all session data for safety. See renew_session.
+  """
+  def log_out_expired_user(conn) do
+    user_token = get_session(conn, :user_token)
+    user_token && Patients.delete_session_token(user_token)
+
+    if live_socket_id = get_session(conn, :live_socket_id) do
+      TurnStileWeb.Endpoint.broadcast(live_socket_id, "disconnect", %{})
+    end
+
+    conn
+    |> renew_session()
+    |> delete_resp_cookie(@expiration_cookie)
+  end
+
+  @doc """
+  Authenticates the user by looking into the session
+  - Checks for existence only; no expiration check here - keeping the flow simple
+  """
+  def fetch_current_user(conn, _opts) do
+    {user_token, conn} = ensure_user_session_token(conn)
+    # IO.inspect(user_token, label: "fetch_current_user USER TOKEN")
+    # - queries for exists and valid
+    user = user_token && Patients.confirm_user_session_token_exists(user_token)
+    # IO.inspect(user, label: "fetch_current_user USER")
+    assign(conn, :current_user, user)
+  end
+
+  # first run: looks up user by email token; should fail b/c no user in seesion yet
+  defp ensure_user_session_token(conn) do
+    # check for user token
+    if user_token = get_session(conn, :user_token) do
+      {user_token, conn}
+    else
+      {nil, conn}
+    end
+  end
+
+  @doc """
+  Used for routes that require the user to not be authenticated.
+  """
+  def redirect_if_user_is_authenticated(conn, _opts) do
+    if conn.assigns[:current_user] do
+      IO.puts("Redirect if user is authenticated: redirecting")
+
+      conn
+      |> redirect(to: signed_in_main_path(conn, conn.assigns[:current_user]))
+      |> halt()
+    else
+      IO.puts("Redirect if user is authenticated: not authenicated")
+      conn
+    end
+  end
+
+  def handle_validate_session_token(conn) do
+    if conn.assigns[:current_user] do
+      user_token = get_session(conn, :user_token)
+
+      case user_token && Patients.confirm_user_session_token(user_token) do
+        {:ok, _user} ->
+          IO.puts("handle_validate_session_token: user session not expired")
+          conn
+        {nil, :not_found} ->
+          IO.puts("handle_validate_session_token: user session error")
+          conn
+          |> put_flash(
+            :info,
+            "You're session has encountered a matching error. Contact your provider if you need a new link."
+          )
+          |> log_out_expired_user()
+          |> redirect(to: Routes.page_path(conn, :index))
+          |> halt()
+
+        {:expired, user} ->
+          IO.puts("handle_validate_session_token: user session expired")
+          push_user_and_interface_updates(conn, user.id)
+          conn
+          |> put_flash(
+            :info,
+            "You're session has expired. Contact your provider again to receive a new link."
+          )
+          |> log_out_expired_user()
+          |> redirect(to: Routes.page_path(conn, :index))
+          |> halt()
+      end
+    else
+      conn
+    end
+  end
+
+  @doc """
+  # handle_validate_email_token
+  # - handles request if user if not logged yet
+  # - if logged in user calls this redirect them to new/1
+  # - else confirm email token + log in and redirect to new/1
+  # - if not confirmed email send err val and redirect
+  # - handles all cases of email token errors
+  # - if email token expired sends status update to UI
+  """
+  def handle_validate_email_token(conn, %{"user_id" => user_id, "token" => encoded_token}) do
+    current_user = conn.assigns[:current_user]
+
+    if current_user do
+      conn
+      |> redirect(to: Routes.user_session_path(conn, :new, current_user.id))
+    else
+      # check URL encoded_token - match url to hashed-token in DB
+      case Patients.confirm_user_email_token(encoded_token, user_id) do
+        {:ok, user} ->
+          # IO.inspect(user, label: "USER")
+          conn
+          # log in and set session token for future requests
+          |> log_in_user(user)
+          |> redirect(to: signed_in_main_path(conn, user) || get_session(conn, :user_return_to))
+
+        {nil, :not_matched} ->
+          # user does match but - url :id is not correct for user token
+          IO.puts("user_auth: user not_matched: User param :id does not match the token user id")
+
+          conn
+          |> put_flash(
+            :error,
+            "Sorry, your URL link has errors. Verify it is correct and try again, or contact your provider for a new link."
+          )
+          |> redirect(to: "/")
+
+        {nil, :not_found} ->
+          # no users matching - b/c user session does not match any users
+          IO.puts("user_auth:  not_found")
+
+          conn
+          |> put_flash(:error, "Sorry, your URL link is invalid.")
+          |> redirect(to: "/")
+
+        :invalid_input_token ->
+          # error on func call - b/c user has a malfromed URL i.e. extra quote at end
+          IO.puts("user_auth:  :invalid_input_token")
+
+          conn
+          |> put_flash(
+            :error,
+            "Sorry, your URL link contains errors and is invalid. Confirm it is correct and try again, or contact your provider for a new link."
+          )
+          |> redirect(to: "/")
+
+        {nil, :expired} ->
+          # valid request but expired - will be deleted on this call
+          # fetch full user token struct
+          {:ok, query} = UserToken.encoded_token_and_context_query(encoded_token, "confirm")
+          user_token = TurnStile.Repo.one(query)
+          # delete expirted token
+          Patients.delete_email_token(user_token)
+          IO.puts("user_auth:  token expired and deleted")
+          # update user alert status
+          push_user_and_interface_updates(conn, user_id)
+
+          conn
+          |> put_flash(
+            :error,
+            "Sorry, your link has expired. Contact your provider to resend a new link."
+          )
+          |> redirect(to: "/")
+      end
+    end
+  end
+
+  # push_user_and_interface_updates
+  # - updates user alert status to EXPIRED
+  # - calls Phoenix.PubSub.broadcast to send update to UI
+  defp push_user_and_interface_updates(_conn, user_id) do
+    # update user alert status
+    user = Patients.get_user(user_id)
+
+    {:ok, updated_user} =
+      Patients.update_alert_status(user, UserAlertStatusTypesMap.get_user_status("EXPIRED"))
+
+    # send respnse to update UI; match the DB status - opts pushes it to index liveView handler
+    Phoenix.PubSub.broadcast(
+      TurnStile.PubSub,
+      PubSubTopicsMap.get_topic("STATUS_UPDATE"),
+      %{user_alert_status: updated_user.user_alert_status}
+    )
+  end
+
+  # puts path like /user/:id as :user_return_to
+  defp maybe_store_return_to(%{method: "GET"} = conn) do
+    put_session(conn, :user_return_to, current_path(conn))
+  end
+
+  defp maybe_store_return_to(conn), do: conn
+
+  def signed_in_main_path(conn, current_user),
+    do: Routes.user_session_path(conn, :new, Map.get(current_user, :id))
+end
