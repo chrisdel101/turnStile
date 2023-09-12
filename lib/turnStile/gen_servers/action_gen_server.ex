1f1f3d973d71fb445bdc8b9f3a4d0999f0851ea2
Chris Del <drumgod101@gmail.com>
2023-09-12 11:17:15 -0600

    action gen server basics working

--- lib/turnStile/gen_servers/action_gen_server.ex
+++ lib/turnStile/gen_servers/action_gen_server.ex
@@ -1,44 +1,85 @@
 defmodule TurnStile.ActionGenServer do
   use GenServer
-  alias TurnStile.Patients
-  #client
+  alias TurnStile.Patients.User
+  # client
+
+  # call with client APIs with server name in app code, not pid
 
   def start_link(default) when is_binary(default) do
-    GenServer.start_link(__MODULE__, default)
+    GenServer.start_link(__MODULE__, default, name: :action_server)
   end
 
-  def lookup(pid, name) do
-    GenServer.call(pid, {:lookup, name})
+  # add item to front of list
+  defp unshift(pid, element) do
+    GenServer.cast(pid, {:unshift, element})
   end
-  def push(pid, element) do
-    GenServer.cast(pid, {:push, element})
+  # remove item to front of list
+  defp shift(pid) do
+    GenServer.call(pid, :shift)
   end
 
-  def pop(pid) do
-    GenServer.call(pid, :pop)
+  def print_all(pid) do
+    GenServer.call(pid, :print_all)
+  end
+
+  def delete_all(pid) do
+    GenServer.cast(pid, {:delete_all, nil})
+  end
+  defp lookup(pid, user_id) do
+    GenServer.call(pid, {:lookup, user_id})
+  end
+
+  def add_new_user(pid, user) do
+    case lookup(pid, user.id) do
+      # user is not in list yet
+      nil ->
+        with :ok <- unshift(pid, user) do
+          {:ok, user}
+        end
+        # user already exists
+      %User{} = _user ->
+        msg = "INFO: Cannot add user to state. User already exists in"
+        IO.puts(msg)
+        {:error, msg}
+    end
   end
 
   # Server
 
   @impl true
-  @spec init(binary) :: {:ok, [binary]}
-  def init(:ok) do
-    {:ok, %{}}
+  @spec init(any) :: {:ok, []}
+  def init(_start_string) do
+    schedule_work()
+    {:ok, []}
   end
 
   @impl true
-  def handle_call({:lookup, name}, _from, names) do
-    {:reply, Map.fetch(names, name), names}
+  def handle_call(:shift, _from, state) do
+    [to_caller | new_state] = state
+    IO.inspect(to_caller, label: "to_caller")
+    {:reply, to_caller, new_state}
+  end
+
+  # just prints the list
+  def handle_call(:print_all, _from, state) do
+    {:reply, state, state}
+  end
+
+  # check if user in list
+  def handle_call({:lookup, user_id}, _from, state) do
+    found_user = Enum.find(state, fn user -> user.id === user_id end)
+    {:reply, found_user, state}
   end
 
   @impl true
-  def handle_cast({:create, name}, names) do
-    if Map.has_key?(names, name) do
-      {:noreply, names}
-    else
-      {:ok, bucket} = KV.Bucket.start_link([])
-      {:noreply, Map.put(names, name, bucket)}
-    end
+  def handle_cast({:unshift, element}, state) do
+    new_state = [element | state]
+    {:noreply, new_state}
+  end
+
+  def handle_cast({:delete_all, nil}, _state) do
+    new_state = []
+    {:noreply, new_state}
   end
 
   @impl true
@@ -55,6 +96,6 @@ defmodule TurnStile.ActionGenServer do
   defp schedule_work do
     # We schedule the work to happen in 2 hours (written in milliseconds).
     # Alternatively, one might write :timer.hours(2)
-    Process.send_after(self(), :work, 2 * 60 * 60 * 1000)
+    # Process.send_after(self(), :work, 2 * 60 * 60 * 1000)
   end
 end
